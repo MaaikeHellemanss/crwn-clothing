7204f0a022b5ecdd29c31dac86494897a861a21c
Maaike <mhlm@foreach.be>
2021-02-18 21:18:30 +0100

    Add sign in and sign up

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import './sign-in-and-sign-up.style.scss'
+
+const SignAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        SIGN IN
+        <SignIn/>
+    </div>
+)
+
+export default SignAndSignUpPage;
